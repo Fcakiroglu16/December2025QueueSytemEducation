c5ae267ed4d8aaff25135059824455761ecccbfe
fcakiroglu16 <f-cakiroglu@outlook.com>
2026-01-05 21:22:04 +0300

    Add dead-letter exchange with requeue and consumer logic
    Introduce DeadLetterExchangeExampleWithRequeueMethod to demonstrate dead-lettering with message TTL and delivery limits. Add a Consumer method for processing and acknowledging messages from the main queue. Update Program.cs to invoke both dead-letter example methods and the consumer. Improve code clarity with minor formatting and comment changes.

--- DeadLetterExhangeExampleConsole.App/Program.cs
+++ DeadLetterExhangeExampleConsole.App/Program.cs
@@ -2,8 +2,119 @@
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 
-await DeadLetterExchangeExampleMethod();
+//await DeadLetterExchangeExampleMethod();
 
+await DeadLetterExchangeExampleMethod();
+await DeadLetterExchangeExampleWithRequeueMethod();
+await Consumer();
+Console.ReadLine();
+async Task DeadLetterExchangeExampleWithRequeueMethod()
+{
+    Console.WriteLine("Dead Letter Exchange With Requeue");
+
+
+    const string mainExchange = "main.exchange";
+    const string mainQueue = "main.queue";
+
+    const string deadLetterExchange = "dead.letter.exchange";
+    const string deadLetterQueue = "dead.letter.queue";
+
+    ConnectionFactory connectionFactory = new ConnectionFactory
+    {
+        HostName = "localhost"
+    };
+
+
+    using IConnection connection = await connectionFactory.CreateConnectionAsync();
+
+    using IChannel channel = await connection.CreateChannelAsync();
+
+
+
+    await channel.ExchangeDeclareAsync(mainExchange, ExchangeType.Fanout, true, false, null);
+
+
+    // RabbitMQ Queue Types
+
+
+    Dictionary<string, object> arguments = new Dictionary<string, object>()
+    {
+        {"x-queue-type","quorum"},
+        { "x-dead-letter-exchange", deadLetterExchange },
+        {"x-message-ttl",10000},
+        {"x-delivery-limit",3}
+    };
+
+    await channel.QueueDeclareAsync(mainQueue, true, false, false, arguments);
+
+
+
+
+
+    await channel.QueueBindAsync(mainQueue, mainExchange, string.Empty, null);
+
+
+    // Declare Dead Letter Exchange and Queue
+
+    await channel.ExchangeDeclareAsync(deadLetterExchange, ExchangeType.Fanout, true, false, null);
+
+    await channel.QueueDeclareAsync(deadLetterQueue, true, false, false, null);
+
+    await channel.QueueBindAsync(deadLetterQueue, deadLetterExchange, string.Empty, null);
+
+
+    // send message to main exchange
+    string messageBody = "Hello, World!";
+
+
+    BasicProperties properties = new BasicProperties();
+
+    properties.Persistent = true;
+    //properties.Expiration = "10000"; // 10 saniye (milisaniye cinsinden)
+
+
+    byte[] body = System.Text.Encoding.UTF8.GetBytes(messageBody);
+    await channel.BasicPublishAsync(mainExchange, string.Empty, true, body);
+
+
+    Console.WriteLine("Message sent to main exchange. It will be dead-lettered after TTL expires if not consumed.");
+
+}
+
+async Task Consumer()
+{
+    ConnectionFactory connectionFactory = new ConnectionFactory
+    {
+        HostName = "localhost"
+    };
+
+    using IConnection connection = await connectionFactory.CreateConnectionAsync();
+
+    using IChannel channel1 = await connection.CreateChannelAsync();
+    AsyncEventingBasicConsumer consumer = new AsyncEventingBasicConsumer(channel1);
+    consumer.ReceivedAsync += async (sender, eventArgs) =>
+    {
+
+        Console.WriteLine($"Message Processing-{eventArgs.DeliveryTag}");
+
+        try
+        {
+            string receivedMessage = System.Text.Encoding.UTF8.GetString(eventArgs.Body.ToArray());
+            // Acknowledge the message
+            await channel1.BasicAckAsync(eventArgs.DeliveryTag, false);
+            Console.WriteLine($"Message Acknowledged-{eventArgs.DeliveryTag}");
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            await channel1!.BasicNackAsync(eventArgs.DeliveryTag, false, true);
+        }
+
+    };
+
+    await channel1.BasicConsumeAsync("main.queue", false, consumer);
+}
 async Task DeadLetterExchangeExampleMethod()
 {
     Console.WriteLine("Dead Letter Exchange");
@@ -45,7 +156,7 @@ async Task DeadLetterExchangeExampleMethod()
     await channel.QueueBindAsync(mainQueue, mainExchange, string.Empty, null);
 
 
-// Declare Dead Letter Exchange and Queue
+    // Declare Dead Letter Exchange and Queue
 
     await channel.ExchangeDeclareAsync(deadLetterExchange, ExchangeType.Fanout, true, false, null);
 
@@ -54,14 +165,14 @@ async Task DeadLetterExchangeExampleMethod()
     await channel.QueueBindAsync(deadLetterQueue, deadLetterExchange, string.Empty, null);
 
 
-// send message to main exchange
+    // send message to main exchange
     string messageBody = "Hello, World!";
 
 
     BasicProperties properties = new BasicProperties();
 
     properties.Persistent = true;
-//properties.Expiration = "10000"; // 10 saniye (milisaniye cinsinden)
+    //properties.Expiration = "10000"; // 10 saniye (milisaniye cinsinden)
 
 
     byte[] body = System.Text.Encoding.UTF8.GetBytes(messageBody);
@@ -76,7 +187,7 @@ async Task DeadLetterExchangeExampleMethod()
     {
         try
         {
-            var receivedMessage = System.Text.Encoding.UTF8.GetString(eventArgs.Body.ToArray());
+            string receivedMessage = System.Text.Encoding.UTF8.GetString(eventArgs.Body.ToArray());
             Console.WriteLine($"Received message from dead-letter queue: {receivedMessage}");
             // Acknowledge the message
             await channel.BasicAckAsync(eventArgs.DeliveryTag, false);
@@ -91,5 +202,7 @@ async Task DeadLetterExchangeExampleMethod()
 
     await channel.BasicConsumeAsync(deadLetterQueue, false, consumer);
 }
+
+
 
 
